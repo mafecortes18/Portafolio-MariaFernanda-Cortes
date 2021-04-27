167ba4b4db5a1e6fc7c9fde9b9e8b1c6841a6d71
mafecortes18 <73893765+mafecortes18@users.noreply.github.com>
2021-04-27 10:17:15 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,55 @@
+function contActive() {
+    var selector=document.getElementById("cont-colors");
+    var selector2= document.getElementById("btn-elemento");
+    selector.classList.toggle("active");
+    selector2.classList.toggle("active");
+    
+    }
+    
+function bgazul() {
+    var selector=document.getElementById("body");
+    selector.className='';
+    selector.classList.toggle("azul");
+ 
+    
+}
+function bgamarillo() {
+    var selector=document.getElementById("body");
+    selector.className='';
+    selector.classList.toggle("amarillo");
+ 
+    
+}
+function bglila() {
+    var selector=document.getElementById("body");
+    selector.className='';
+    selector.classList.toggle("lila");
+ 
+    
+}
+function bgnegro() {
+    var selector=document.getElementById("body");
+    selector.className='';
+    selector.classList.toggle("negro");
+ 
+    
+}
+function bgverde() {
+    var selector=document.getElementById("body");
+    selector.className='';
+    selector.classList.toggle("verde");
+ 
+    
+}
+function bgrosa() {
+    var selector=document.getElementById("body");
+    selector.className='';
+    selector.classList.toggle("rosa");
+ 
+    
+}
+
+
+
+
+AOS.init();
